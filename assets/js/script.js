814da149b7cae155ed75ea25c8853885aa4db441
Odney Bleus <odney@Odneys-MacBook-Air.local>
2024-04-11 11:31:57 -0400

    added alert if task name and type is empty and also to clear form once submited

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,13 @@ var taskFormHandler = function() {
    var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
+  
   };
+  if(!taskNameInput || !taskTypeInput){
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
